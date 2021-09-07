efc865a6c82ead3fb2ac00f8988187150c0e2ee5
Jan Cizmar <cizmar@chlupac.com>
2021-09-07 16:05:36 +0200

    Initial commit from Create Next App

---
+++ next.config.js
@@ -0,0 +1,9 @@
+/** @type {import('next').NextConfig} */
+module.exports = {
+    reactStrictMode: true,
+    i18n: {
+        locales: ['en', 'cs'],
+        localeDetection: true,
+        defaultLocale: "en"
+    },
+}
