31fcb4822425593a425f0afbb9f77ac4c88908c8
Tolgee Automation Machine <machine@tolgee.io>
2021-11-16 09:32:22 +0000

    chore: Publishing example from release:

--- next.config.js
+++ next.config.js
@@ -1,9 +1,10 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
-    reactStrictMode: true,
-    i18n: {
-        locales: ['en', 'cs'],
-        localeDetection: true,
-        defaultLocale: "en"
-    },
-}
+  reactStrictMode: true,
+  i18n: {
+    locales: ['en', 'de', 'fr', 'cs'],
+    localeDetection: true,
+    defaultLocale: 'en',
+  },
+  distDir: process.env.NEXT_BUILD_DIR || '.next',
+};
